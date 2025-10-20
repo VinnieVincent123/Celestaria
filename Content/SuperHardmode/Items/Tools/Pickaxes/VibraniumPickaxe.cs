13bf3d541e427ec66c89561b9bbd85eadd11e927
Mr. Callous <thomasgarjung@web.de>
2025-10-20 16:21:02 +0200

    son
    SuperHardmode Pickaxes have been made significantly faster

--- Content/SuperHardmode/Items/Tools/Pickaxes/VibraniumPickaxe.cs
+++ Content/SuperHardmode/Items/Tools/Pickaxes/VibraniumPickaxe.cs
@@ -11,8 +11,8 @@ namespace WeBall.Content.SuperHardmode.Items.Tools.Pickaxes
             Item.width = 30;
             Item.height = 30;
             Item.damage = 60;
-            Item.useAnimation = 15;
-            Item.useTime = 15;
+            Item.useAnimation = 5;
+            Item.useTime = 5;
             Item.pick = 240;
             Item.tileBoost += 5;
             Item.knockBack = 5f;
