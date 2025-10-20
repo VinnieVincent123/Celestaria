f29a70f39b81099e816516544a03e17ea3a0e3e2
Mr. Callous <thomasgarjung@web.de>
2025-10-20 16:22:50 +0200

    cool new tiles

---
+++ Content/SuperHardmode/Tiles/Bars/VibraniumBarTile.cs
@@ -0,0 +1,22 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace WeBall.Content.SuperHardmode.Tiles.Bars
+{
+    public class VibraniumBarTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileShine[Type] = 1100;
+            Main.tileShine2[Type] = true;
+            Main.tileLighted[Type] = true;
+
+            AddMapEntry(new Color(80, 180, 255), CreateMapEntryName());
+            DustType = DustID.Platinum;
+            HitSound = SoundID.Tink;
+        }
+    }
+}
---
+++ Content/SuperHardmode/Tiles/Ores/UruOreTile.cs
@@ -0,0 +1,29 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using WeBall.Content.SuperHardmode.Items.Materials.Ores;
+
+namespace WeBall.Content.SuperHardmode.Tiles.Ores
+{
+    public class UruOreTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileSpelunker[Type] = true;
+            Main.tileShine[Type] = 1100;
+            Main.tileShine2[Type] = true;
+            Main.tileOreFinderPriority[Type] = 700;
+            Main.tileLighted[Type] = true;
+
+            MineResist = 5f;
+            MinPick = 240;
+
+            AddMapEntry(new Color(80, 180, 255), CreateMapEntryName());
+            DustType = DustID.Platinum;
+            HitSound = SoundID.Tink;
+        }
+    }
+}
