bf5ee1ce74c0238e53b58874e14401f82277e2cf
Mr. Callous <thomasgarjung@web.de>
2025-10-20 15:24:23 +0200

    SuperHardmode Ores

---
+++ Content/SuperHardmode/Items/Materials/Ores/BeskarOre.cs
@@ -0,0 +1,25 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WeBall.Content.SuperHardmode.Tiles.Ores;
+
+namespace WeBall.Content.SuperHardmode.Items.Materials.Ores
+{
+    public class BeskarOre : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 12;
+            Item.height = 12;
+            Item.maxStack = 9999;
+            Item.value = Item.sellPrice(gold: 1);
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTurn = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.autoReuse = true;
+            Item.consumable = true;
+            Item.createTile = ModContent.TileType<BeskarOreTile>(); 
+        }
+    }
+}
