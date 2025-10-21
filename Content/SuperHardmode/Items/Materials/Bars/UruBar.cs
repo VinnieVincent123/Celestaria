6953069b26ea148a3fe12988573c4ed3e917b9b0
Mr. Callous <thomasgarjung@web.de>
2025-10-21 19:45:30 +0200

    SuperHardmode Bars

---
+++ Content/SuperHardmode/Items/Materials/Bars/UruBar.cs
@@ -0,0 +1,25 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WeBall.Content.SuperHardmode.Tiles.Bars;
+
+namespace WeBall.Content.SuperHardmode.Items.Materials.Bars
+{
+    public class UruBar : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 12;
+            Item.height = 12;
+            Item.maxStack = 9999;
+            Item.value = Item.sellPrice(gold: 1);
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTurn = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.autoReuse = true;
+            Item.consumable = true;
+            Item.createTile = ModContent.TileType<UruBarTile>(); 
+        }
+    }
+}
